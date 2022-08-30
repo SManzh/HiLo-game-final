287990883faa1f26e6d06196d79ae1b1b8414174
Vyacheslav Manzhela <slavamanzh@gmail.com>
2022-08-30 19:50:47 +0300

    feat: enlarge fonts, change color, solve bag with lose-scenario

--- src/GuessingGame.java
+++ src/GuessingGame.java
@@ -9,6 +9,7 @@ import javax.swing.JButton;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
 import javax.swing.JTextField;
+import java.awt.Color;
 
 public class GuessingGame extends JFrame {
 
@@ -28,7 +29,6 @@ public class GuessingGame extends JFrame {
 		try {
 			int guess = Integer.parseInt(guessText);
 			if (numberOfTries>=0) {
-			
 				if (guess < theNumber) {
 					message = guess + " is too low. Try again.";
 				} else if (guess > theNumber) {
@@ -40,6 +40,7 @@ public class GuessingGame extends JFrame {
 				}	
 			} else {
 				message = "You lose";
+				newGame();
 			}
 			
 		} catch (Exception e) {
@@ -58,18 +59,19 @@ public class GuessingGame extends JFrame {
 	}
 
 	public GuessingGame() {
+		getContentPane().setBackground(Color.LIGHT_GRAY);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setTitle("Hi-Lo Guessing Game");
 		getContentPane().setLayout(null);
 
 		JLabel lblNewLabel = new JLabel("Slava's Hi-Lo Guessing Game");
-		lblNewLabel.setFont(new Font("Tahoma", Font.BOLD, 15));
+		lblNewLabel.setFont(new Font("Tahoma", Font.BOLD, 16));
 		lblNewLabel.setHorizontalAlignment(SwingConstants.CENTER);
 		lblNewLabel.setBounds(5, 36, 566, 19);
 		getContentPane().add(lblNewLabel);
 
 		JLabel lblNewLabel_1 = new JLabel("Guess number between -99 and 100");
-		lblNewLabel_1.setFont(new Font("Tahoma", Font.PLAIN, 14));
+		lblNewLabel_1.setFont(new Font("Tahoma", Font.PLAIN, 15));
 		lblNewLabel_1.setHorizontalAlignment(SwingConstants.RIGHT);
 		lblNewLabel_1.setBounds(77, 91, 248, 22);
 		getContentPane().add(lblNewLabel_1);
@@ -80,12 +82,12 @@ public class GuessingGame extends JFrame {
 				checkGuess();
 			}
 		});
-		btnNewButton.setFont(new Font("Tahoma", Font.PLAIN, 14));
+		btnNewButton.setFont(new Font("Tahoma", Font.PLAIN, 15));
 		btnNewButton.setBounds(245, 149, 85, 22);
 		getContentPane().add(btnNewButton);
 
 		lblOutput = new JLabel("Enter a number above and click Guess!");
-		lblOutput.setFont(new Font("Tahoma", Font.PLAIN, 14));
+		lblOutput.setFont(new Font("Tahoma", Font.PLAIN, 15));
 		lblOutput.setHorizontalAlignment(SwingConstants.CENTER);
 		lblOutput.setBounds(0, 207, 576, 19);
 		getContentPane().add(lblOutput);
